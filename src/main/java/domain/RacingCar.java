e165a2e605347c124347f49cf3eeecdae4242339
JisuLim <sanola3@gmail.com>
2021-05-04 15:31:56 +0900

    feat: add setName, validateName methods to RacingCar class

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -2,6 +2,14 @@ package domain;
 
 public class RacingCar {
 	private int moveCount = 0;
+	private String name;
+
+	public RacingCar() {
+	}
+
+	public RacingCar(String carName) {
+		this.setName(carName);
+	}
 
 	public int getMoveCount() {
 		return moveCount;
@@ -10,6 +18,21 @@ public class RacingCar {
 	public void move(int randomNumber) {
 		if (randomNumber >= 4) {
 			this.moveCount++;
+		}
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	private void setName(String inputName) {
+		this.validateName(inputName);
+		this.name = inputName;
+	}
+
+	private void validateName(String inputName) {
+		if (inputName.length() > 5) {
+			throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다");
 		}
 	}
 }
--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -2,10 +2,12 @@ package domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarTest {
 	private RacingCar racingCar;
@@ -31,5 +33,23 @@ public class RacingCarTest {
 		int previousCarMoveCount = racingCar.getMoveCount();
 		racingCar.move(randomNumber);
 		assertThat(racingCar.getMoveCount()).isEqualTo(previousCarMoveCount);
+	}
+
+	@Test
+	@DisplayName("자동차는 이름을 가질 수 있다")
+	void name() {
+		String carName = "bmw";
+		RacingCar bmw = new RacingCar(carName);
+		assertThat(bmw.getName()).isEqualTo(carName);
+	}
+
+	@Test
+	@DisplayName("자동차는 5자 이하의 이름만 가질 수 있다")
+	void nameLimitedUnder5() {
+		String carName = "ferrari";
+		assertThatThrownBy(() -> new RacingCar(carName))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("자동차 이름은 5자 이하만 가능합니다");
+
 	}
 }
